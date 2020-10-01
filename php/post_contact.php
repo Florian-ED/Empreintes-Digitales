a4211623bb3b5330afc3d124deb6e3df939c1cc1
Florian <contact@empreintesdigitales.fr>
2020-10-01 16:11:08 +0200

    Modif php

--- php/post_contact.php
+++ php/post_contact.php
@@ -13,9 +13,9 @@ if(!array_key_exists('tel', $_POST) || $_POST['tel'] == ''){
     $errors['tel'] = "Vous n'avez pas rentré votre numéro de téléphone";
 }
 
-if(!array_key_exists('service', $_POST) || $_POST['service'] != "1" || $_POST['service'] != "2" || $_POST['service'] != "0"){
+/*if(!array_key_exists('service', $_POST) || $_POST['service'] != "1" || $_POST['service'] != "2" || $_POST['service'] != "0"){
     $errors['service'] = "Vous n'avez pas choisi de service";
-}
+}*/
 
 if(!array_key_exists('message', $_POST) || $_POST['message'] == ''){
     $errors['message'] = "Vous n'avez pas rentré votre message";
@@ -42,9 +42,9 @@ else{
             $nameService = "Autres";
             break;
     }
-    $message = "Service choisi :" . $nameService . "\r\n\r\n" . $_POST['message'] . "\r\n\r\n" . "Numéro de téléphone: " . $_POST['tel'];
-    $headers = 'De: ' . $_POST['email'];
-    mail('contact@empreintesdigitales.fr', 'Formulaire de contact', $message, $headers);
+    $message = "Service choisi :" . $nameService . "\r\n\r\n" . $_POST['message'] . "\r\n\r\n" . "Numéro de téléphone: " . $_POST['tel'] . "\r\n\r\n" . "Adresse Mail: " . $_POST['email'];
+    $headers = "FROM: " . $_POST['email'];
+    mail('empreintesdigitales56@gmail.com', 'Formulaire de contact', $message, $headers);
     header('Location: ../contact.php');
 }
 
